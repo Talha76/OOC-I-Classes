b7bc0907bf9297a2b5b0912ac31fa150cfa6e3f3
Mushfiq Talha <58101262+Talha76@users.noreply.github.com>
2023-04-30 16:58:19 +0600

    Uploaded FileStream ErrorHandling Generics

---
+++ FileStreamErrorHandlingGenerics/DynamicArray.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileStreamErrorHandlingGenerics
+{
+    internal class DynamicArray<T>
+    {
+        private int size;
+        private T[] array;
+
+        public DynamicArray()
+        {
+            size = 0;
+            array = new T[0];
+        }
+
+        public DynamicArray(int size)
+        {
+            this.size = size;
+            array = new T[size];
+        }
+
+        public DynamicArray(List<T> list)
+        {
+            size = list.Count;
+            array = new T[size];
+            for (int i = 0; i < size; i++)
+                array[i] = list[i];
+        }
+
+        public void Push(T value)
+        {
+            array[size] = value;
+            size++;
+        }
+
+        public T Pop()
+        {
+            if (size == 0)
+                throw new ApplicationException("Cannot pop from empty array");
+            size--;
+            return array[size];
+        }
+
+        public int getSize() { return size; }
+
+        public T getElement(int position)
+        {
+            if (position < 0)
+                throw new ApplicationException("Array index cannot be negative");
+            if (position >= size)
+                throw new ApplicationException("Can't fetch element at position greater than size");
+            return array[position];
+        }
+    }
+}
---
+++ FileStreamErrorHandlingGenerics/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileStreamErrorHandlingGenerics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /******* Generic Demo ************/
+            //List<int> list = new List<int>();
+            //list.Add(1); list.Add(3); list.Add(2);
+
+            //DynamicArray<int> dynArray = new DynamicArray<int>(list);
+            //for (int i = 0; i < 3; i++)
+            //    Console.WriteLine(dynArray.getElement(i));
+
+            /********** Error handling **********/
+            //DynamicArray<int> array = new DynamicArray<int>();
+
+            //try
+            //{
+            //    Console.WriteLine("Before Pop.");
+            //    Console.WriteLine(array.Pop());
+            //    Console.WriteLine("After Pop");
+            //}
+            //catch (Exception ex) { Console.WriteLine(ex.Message); }
+
+            /********** File Stream Read/Write *************/
+            //using (StreamReader sr = new StreamReader("E:/OOC-I-Classes/README.md"))
+            //{
+            //    while (!sr.EndOfStream) { Console.WriteLine(sr.ReadLine()); }
+            //    sr.Close();
+            //}
+
+            using (StreamWriter sr = new StreamWriter("E:/OOC-I-Classes/written.txt"))
+            {
+                sr.WriteLine("This is a new file.");
+                sr.Close();
+            }
+
+            using (StreamReader sr = new StreamReader("E:/OOC-I-Classes/written.txt"))
+            {
+                while (!sr.EndOfStream) Console.WriteLine(sr.ReadLine());
+                sr.Close();
+            }
+        }
+    }
+}
